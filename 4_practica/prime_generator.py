298ac1f3e5b2e93f5838ae90c1d9aa3d9297318b
Belu Ticona <ticona.belu@gmail.com>
2020-03-26 11:25:16 -0300

    Subo p4

---
+++ 4_practica/prime_generator.py
@@ -0,0 +1,29 @@
+from random import randint
+from random import randrange
+
+def big_int(size=None):
+    """
+    Generador de números aleatorios de un tamaño fijo recibido como parámetro, si el parámetro es
+    menor que 100 o None, entonces la función no le hace caso y genera uno de tamaño arbitrario,
+    máximo es de 150 dígitos.
+    :return: Un número del tamaño descrito.
+    """
+    pass
+
+def miller_rabin(n):
+    """
+    Implementación del test de primalidad de Miller-Rabin.
+    :param n: El número a determinar su primalidad.
+    :return: True si n es primo, False en otro caso.
+    """
+    pass
+
+
+def wilson(n):
+    """
+    Implementaión del test de primalidad de Wilson, basado en el teorema de Wilson,
+    (p-1)! ≡ -1 mod p
+    :param n: El número a determinar su primalidad.
+    :return: True si n es primo, False en otro caso.
+    """
+    pass
